82d7ec6c7aaf0a541d9ae11344f0f75e97996117
LeKe <95280282+goflutterjava@users.noreply.github.com>
2022-07-06 14:12:59 +0800

    fix integrate-tests pulsar not enable broker entry meta (#96)

--- docker-build/scripts/start.sh
+++ docker-build/scripts/start.sh
@@ -20,7 +20,7 @@
 
 export PULSAR_ALLOW_AUTO_TOPIC_CREATION_TYPE=partitioned
 export PULSAR_BROKER_ENTRY_METADATA_INTERCEPTORS=org.apache.pulsar.common.intercept.AppendIndexMetadataInterceptor
-export PULSAR_EXPOSING_BROKER_ENTRY_METADATA_TO_CLIENT=true
+export PULSAR_EXPOSING_BROKER_ENTRY_METADATA_TO_CLIENT_ENABLED=true
 export REMOTE_MODE=false
 bash -vx /opt/sh/pulsar/mate/scripts/start-daemon.sh
 sleep 60
